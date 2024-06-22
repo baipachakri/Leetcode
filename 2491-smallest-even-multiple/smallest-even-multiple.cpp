1899b6385712bafe19e7726c44b2e98969ba0146
Chakri Baipa <115349256+baipachakri@users.noreply.github.com>
2024-06-22 18:10:59 +0530

    Time: 3 ms (33.80%) | Memory: 7.1 MB (39.87%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
+       while(1){
         if(n%2 ==0){
             return n;
         }
         else{
         return n*2;}
+    }
     }
 };
