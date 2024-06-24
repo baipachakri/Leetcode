e73fa8263b65eaad073a3b7cdcb6c0aa79ef426b
Chakri Baipa <115349256+baipachakri@users.noreply.github.com>
2024-06-24 17:23:23 +0530

    Time: 2 ms (7.95%) | Memory: 45.7 MB (9.19%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,9 +1,14 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int [] result = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            result[i]= nums[nums[i]];
+        int CONST = 1001;
+        for(int i=0;i<nums.length ;i++){
+            int a = nums[i];
+            int b = nums[a]%CONST;
+            nums[i]=a+b*CONST;
         }
-        return result;
+        for(int t=0;t<nums.length;t++){
+            nums[t]= nums[t]/CONST;
+        }
+        return nums;
     }
 }
